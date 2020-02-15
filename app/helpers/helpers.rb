50539d095fd020c472efbfc153bae9f8bd893252
Riley Slayden <slaydenriley@gmail.com>
2020-02-15 18:03:47 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Helpers
-  def self.current_user(hash)
-    @user = User.find_by_id(:user_id => hash[:user_id])
+  def self.current_user(session)
+    @user = User.find_by_id(:user_id => session[:user_id])
   end
 
   def self.is_logged_in?
